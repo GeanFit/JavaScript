afc60733f50cc1aeea4afeb4a43e0a49b6eaf27f
Gean Oliveira <126193157+GeanFit@users.noreply.github.com>
2023-06-09 23:24:36 -0300

    atualizações

---
+++ aula013/node_4eb6f85b79fd8.tmp.js
@@ -0,0 +1,21 @@
+var c = 1
+    do{
+        console.log(`Passo ${c}`)
+    }while(c <=6)
+
+
+/*
+var c = 1
+    while (c <=6){
+        console.log(`Passo ${c}`)
+        c++
+    }
+*/
+
+/*
+console.log('Passo 01')
+console.log('Passo 02')
+console.log('Passo 03')
+console.log('Passo 04')
+*/
+
