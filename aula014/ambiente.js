4feb8e8211a7a9b33ece1de8a748320c798a25f2
Gean Oliveira <126193157+GeanFit@users.noreply.github.com>
2023-06-11 22:38:19 -0300

    ATUALIZAÇÕES

---
+++ aula014/ambiente.js
@@ -0,0 +1,27 @@
+/*
+SANDO WHILE:
+
+var c = 1
+while (c <= 10){
+    console.log(c)
+    c++
+}
+*/
+
+
+/*
+SANDO DO:
+
+var c = 1
+do {
+    console.log(c)
+    c++
+} while ( c <= 10)
+*/
+
+// USANDO FOR:
+
+
+for (var c = 1; c <= 10; c++){
+    console.log(c)
+}
---
+++ aula014/node_df4349dc90f79.tmp.js
@@ -0,0 +1,11 @@
+/*
+var c = 1
+while (c <= 10){
+    console.log(c)
+    c++
+}
+*/
+
+for (var c = 1; c <= 1000000000; c++){
+    console.log(c)
+}
