bc4d4ad4e69a724b2e31d1b1ed1b17edbf7c8540
Gean Oliveira <126193157+GeanFit@users.noreply.github.com>
2023-11-22 16:48:10 -0300

    atualizações

--- aula049/aula049.js
+++ aula049/aula049.js
@@ -2,11 +2,11 @@ const p_array=document.querySelector("#array")
 const btnVerificar=document.querySelector("#btnVerificar")
 const resultado=document.querySelector("resultado")
 
-const elementos_array=[21,25,14,20,28,18,22]
+const elementos_array=[01,10,14,13,12,15,11]
 p_array.innerHTML="["+elementos_array+"]"
 
 btnVerificar.addEventListener("click",(evt)=>{
-    const ret=elementos_array.every((e,i)=>{
+    const ret=elementos_array.some((e,i)=>{
         if(e<18){
             resultado.innerHTML="Array não conforme na posição " +i
         }
